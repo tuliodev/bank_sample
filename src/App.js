8bd3127cd1b4fb04d051047f13ed51496f2fbc51
tuliodev <resendetulio51@gmail.com>
2023-09-29 12:24:27 -0300

    feature: add global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { ThemeProvider, createMuiTheme } from '@mui/material';
 import React from 'react';
 import Home from './pages/Home';
 
+import './assets/css/Global.css';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Oswald',
+      'sans-serif',
+    ].join(','),
+  },
+});
+
 function App() {
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+
   );
 }
 
