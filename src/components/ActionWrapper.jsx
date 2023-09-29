7f3952a63e73f3690263a25c79071e2d39d4cf0f
tuliodev <resendetulio51@gmail.com>
2023-09-29 12:25:31 -0300

    Feature: add ActionWrapper component

---
+++ src/components/ActionWrapper.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Box,
+} from '@mui/material';
+
+export default function ActionWrapper() {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+      }}
+    >
+      <Box
+        sx={{
+          width: 300,
+          height: 400,
+        }}
+        color="#000"
+      />
+      <Box />
+      <Box />
+      <Box />
+    </Box>
+
+  );
+}
