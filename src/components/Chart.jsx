cf51fe08505a049df6b7bc4feb23ff6d07dbf262
tuliodev <resendetulio51@gmail.com>
2023-09-29 14:19:22 -0300

    Feature: add Chart component

---
+++ src/components/Chart.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler,
+);
+
+export const options = {
+  responsive: true,
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'Bills',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export default function Chart() {
+  return (
+    <>
+      {/* <Typography style={{ fontSize: '5vh'}}>Welcome, Mr. Tulio</Typography> */}
+      <Line options={options} data={data} />
+    </>
+  );
+}
