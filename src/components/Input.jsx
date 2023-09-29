48fe48375fc549eb7ca8b53af7bfab2088b90859
tuliodev <resendetulio51@gmail.com>
2023-09-29 14:19:42 -0300

    feature: add img on Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,18 +3,19 @@ import React from 'react';
 import {
   Container, InputWrapper, InputLabel, StyledInput, Users, UsersAvatar,
 } from '../assets/css/Input';
+import UserImg from '../assets/img/profile.jpeg';
 
 export default function Input() {
   return (
     <Container>
       <InputWrapper>
         <InputLabel>Enter amount</InputLabel>
-        <StyledInput placeholder="$1,24" />
+        <StyledInput disableUnderline placeholder="$1,24" />
       </InputWrapper>
 
       <Users>
-        <UsersAvatar />
-        <UsersAvatar />
+        <UsersAvatar src={UserImg} />
+        <UsersAvatar src={UserImg} />
       </Users>
     </Container>
   );
