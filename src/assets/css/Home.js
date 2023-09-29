efa975e696ce4df87081f9a0450f7aa2adee7130
tuliodev <resendetulio51@gmail.com>
2023-09-29 14:18:43 -0300

    feature: add responsive on home styled component

--- src/assets/css/Home.js
+++ src/assets/css/Home.js
@@ -4,12 +4,25 @@ export const ContentRowDiv = styled.div`
     display: flex; 
     flex-direction: row; 
     justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
     
     
-    @media (max-width: 576px) {
+    @media (max-width: 768px) {
     flex-direction: column;
     justify-content: center;
     align-items: center;
+  }
+`;
+
+export const ChartDiv = styled.div`
+    height: 50vh;
+    width: 50vw;
+    position: relative;
+    margin-top: 13vh;
+
+    @media (max-width: 768px) {
+    display: none;
   }
 `;
 
@@ -26,5 +39,14 @@ export const Container = styled.div`
     display: flex; 
     flex-direction: column; 
     justify-content: space-between;
-    padding-top: 16px;
+
+
+        
+    @media (max-width: 768px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    width: auto;
+  }
 `;
