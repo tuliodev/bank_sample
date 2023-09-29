f21a0af2bde1ac9f7b11a71b2d71d23e302f7921
tuliodev <resendetulio51@gmail.com>
2023-09-29 12:27:03 -0300

    feature: change Home page arquiteture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import MenuAppBar from '../components/MenuAppBar';
+import Layout from '../components/Layout';
 import MobileOpenProvider from '../Contexts/MobileOpen';
+import { Container, ContentColumnDiv, ContentRowDiv } from '../assets/css/Home';
+import CreditCard from '../components/CreditCard';
+import TransferForm from '../components/TransferForm';
+import TransactionsTable from '../components/TransactionsTable';
+import StatisticsList from '../components/StatisticsList';
 
 export default function Home() {
   return (
     <MobileOpenProvider>
-      <MenuAppBar />
+      <Layout>
+        <Container>
+          <ContentRowDiv>
+            <CreditCard />
+            <TransferForm />
+          </ContentRowDiv>
+          <ContentColumnDiv>
+            <TransactionsTable />
+            <StatisticsList />
+          </ContentColumnDiv>
+        </Container>
+      </Layout>
     </MobileOpenProvider>
   );
 }
